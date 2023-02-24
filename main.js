f555a35fcebf3553bdd2b0d206f782d44601f49c
gogolashvili <ggogolashvili91@gmail.com>
2023-02-24 23:52:52 +0400

    documents selected j s

---
+++ main.js
@@ -0,0 +1,5 @@
+let email = document.querySelector("input");
+let button = document.querySelector("button");
+let answer = document.querySelector("p");
+
+let pasuxi = button.addEventListener("click", function () {});
