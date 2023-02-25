6bda141750489b786716e15e1aa5755996f991db
gogolashvili <ggogolashvili91@gmail.com>
2023-02-25 10:55:12 +0400

    add j s function

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ let email = document.querySelector("input");
 let button = document.querySelector("button");
 let answer = document.querySelector("p");
 
-let pasuxi = button.addEventListener("click", function () {});
+let pasuxi = button.addEventListener("click", function () {
+  if (email.value != "@") {
+    answer.innerHTML = "Please provide a valid email";
+  } else answer.innerHTML = "";
+});
