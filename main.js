3d1e2aa2e8b76e6dfdda0a42b64d500690fabf88
gogolashvili <ggogolashvili91@gmail.com>
2023-03-06 10:14:23 +0400

    add function style

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let warning = document.querySelector(".img1");
 button.addEventListener("click", function () {
   if (email.value.includes("@")) {
     answer.innerHTML = "";
+    warning.style.display = "none";
   } else {
     answer.innerHTML = "Please provide a valid email";
     warning.style.display = "block";
